97265e40e08da77590f5c8bc25d511f7a6c4fbc8
Juan Jeldres <jjeldresbiz@gmail.com>
2026-05-08 11:34:49 -0400

    ejercicio_repaso

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # repositorio-de-prueba
+### Ejercicios JavaScript Básico ###
+En este repositorio se encuentran ejercicios de repaso de programacion de la #semana 1 en JavaScript con:
+
+- Variables
+- Operadores
+- Comparaciones
+- Operadores Lógicos
+
+## Autor
+Juan 'PepeBoteia' Jeldres
---
+++ ejercicio_repaso.js
@@ -0,0 +1,59 @@
+/*## Objetivo ##
+- Practicar JavaScript básico (variables, operadores, console.log)
+- Aprender a crear un repositorio en [GitHub](https://github.com/?utm_source=chatgpt.com)
+- Subir código usando Git */
+console.log("### objetivo ###");
+console.log("Practicar JavaScript básico -variables, operadores, console.log- Aprender a crear un repositorio en GitHub *https://github.com/?utm_source=chatgpt.com* Subir código usando Git");
+
+let nombre = "Juan";
+let apellido = "Jeldres";
+let X = 5;
+
+console.log("Hola", nombre, apellido + "."); // ejercicio 1
+
+console.log((X + 5) * 3); // ejercicio 2
+
+let fav_food = "Comida favorita: Helado de menta";
+console.log(fav_food);
+
+let a = 10;
+let b = 3;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a % b); // el resto
+
+a = "10"; //re asignacion, va sin let
+b = "3";
+
+console.log(a + b); // como son string, no se suma si no que queda 103
+console.log(a - b);
+console.log(a * b);
+console.log(a % b);
+
+let edad = 15;
+console.log(edad > 10);
+console.log(edad < 18);
+
+let numero = 8;
+console.log(numero > 5 && numero < 10); //imprime si numero es mayor a 5 y menor a 10
+
+let nota = 6;
+console.log(nota == 7); // imprime si la nota es igual a 7
+
+let temperatura = 25;
+console.log(temperatura >= 30);
+
+let y = 6;
+let z = 9;
+console.log(y == z);
+console.log(z > y);
+
+edad = 17;
+let tienePermiso = true;
+console.log("puede conducir", edad >= 18 || tienePermiso);
+
+let usuario = "admin";
+let password = "1234";
+console.log(usuario === "admin" && password === "1234");
